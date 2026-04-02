e13ec1f8f63ca3b2ae2ef8e23055de4bb5c464de
exentrich <winlinx@yandex.com>
2026-04-02 14:01:50 +0300

    Better error page for no email verify

--- src/pages/_error/+Page.jsx
+++ src/pages/_error/+Page.jsx
@@ -5,7 +5,13 @@ export default function ErrorPage() {
 	const { abortStatusCode, abortReason } = pageContext
 
 	if (abortReason == 'email-not-confirmed')
-		return <h1>Ask public page user to confirm email address</h1>
+		return (
+			<div style={{margin: '3em'}}>
+				<h1>This public page is temporarily unavailable</h1>
+				<p>The owner of this collection hasn't confirmed their email address yet. To prevent spam, public pages are only accessible when the owner's email is verified.</p>
+				<p>If you are the owner of this collection, <a href="https://help.raindrop.io/public-page#why-is-my-public-collection-not-accessible" target="_blank" rel="noreferrer">learn how to fix this</a></p>
+			</div>
+		)
 
 	return (
 		<h1>{abortStatusCode} Error</h1>
